d998336f347a957d2e642bce3318eebddd38f1c4
Hyper Weavers <info@hyperweavers.com>
2025-07-28 17:32:10 +0530

    feat: add fallback apis to handle primary api failures

--- src/jobs/notifier.js
+++ src/jobs/notifier.js
@@ -10,27 +10,84 @@ const { saveTodayRetailPrice } = require('../utils/db-utils');
 require('../utils/axios-utils');
 
 const GOLD_RETAIL_PRICE_API_URL = process.env.GOLD_RETAIL_PRICE_API_URL || '';
+const GOLD_RETAIL_PRICE_API_BACKUP_URL =
+  process.env.GOLD_RETAIL_PRICE_API_BACKUP_URL || '';
 
 const GRAMS_PER_SAVARAN = 8;
 const NEW_LINE = '%0A';
+
+const parsePrimaryApiResponse = (response) => {
+  let price = null;
+
+  if (response.data && response.data['22kt']) {
+    price = response.data['22kt'].match(/[\d]+/g).slice(0, -1).join('');
+
+    console.info(`Today's gold retail price: Rs.${price}/gram`);
+  } else {
+    console.error(`Invalid response data: ${JSON.stringify(response)}`);
+  }
+
+  return price;
+};
+
+const parseSecondaryApiResponse = (response) => {
+  let price = null;
+
+  if (response.data) {
+    const stateWiseGoldPriceList = response.data.data?.getregionalgoldrates;
+
+    if (
+      stateWiseGoldPriceList &&
+      Array.isArray(stateWiseGoldPriceList) &&
+      stateWiseGoldPriceList.length > 0
+    ) {
+      const chennaiPrice = stateWiseGoldPriceList.find(
+        (item) => item.State?.toLowerCase()?.replace(/\s/g, '') === 'tamilnadu'
+      );
+
+      if (chennaiPrice) {
+        price = chennaiPrice.Rate;
+
+        console.info(`Today's gold retail price: Rs.${price}/gram`);
+      } else {
+        console.error(
+          `TN data not found: ${JSON.stringify(stateWiseGoldPriceList)}`
+        );
+      }
+    } else {
+      console.error(`Invalid data format: ${JSON.stringify(response)}`);
+    }
+  } else {
+    console.error(`Invalid response data: ${JSON.stringify(response)}`);
+  }
+
+  return price;
+};
 
 const getRetailPrice = async () => {
   const jar = new CookieJar();
   const client = wrapper(axios.create({ jar }));
 
-  let price = '';
+  let price = null;
 
   try {
     const response = await client.get(GOLD_RETAIL_PRICE_API_URL);
-    if (response.data && response.data['22kt']) {
-      price = response.data['22kt'].match(/[\d]+/g).slice(0, -1).join('');
 
-      console.info(`Today's gold retail price: Rs.${price}/gram`);
-    } else {
-      console.error(`Invalid retail data: ${JSON.stringify(response)}`);
-    }
+    price = parsePrimaryApiResponse(response);
   } catch (error) {
     console.error(`Error fetching retail data: ${JSON.stringify(error)}`);
+  }
+
+  if (!price) {
+    console.info('Primary API failed. Falling back to backup API...');
+
+    try {
+      const response = await client.get(GOLD_RETAIL_PRICE_API_BACKUP_URL);
+
+      price = parseSecondaryApiResponse(response);
+    } catch (error) {
+      console.error(`Error fetching retail data: ${JSON.stringify(error)}`);
+    }
   }
 
   return price;
@@ -57,9 +114,13 @@ const init = async () => {
       console.info('Message sent successfully!');
     } else {
       console.error('Message sending failed!');
+
+      process.exit(1);
     }
   } else {
     console.error('Retail price is not valid!');
+
+    process.exit(1);
   }
 };
 
--- src/jobs/predictor.js
+++ src/jobs/predictor.js
@@ -8,21 +8,29 @@ const { saveTodayMarketPrice } = require('../utils/db-utils');
 require('../utils/axios-utils');
 
 const GOLD_MARKET_PRICE_API_URL = process.env.GOLD_MARKET_PRICE_API_URL || '';
+const GOLD_MARKET_PRICE_API_BACKUP_URL =
+  process.env.GOLD_MARKET_PRICE_API_BACKUP_URL || '';
 
-const getMarketPosition = async () => {
+const parsePrimaryApiResponse = (response) => {
   let position = null;
 
-  try {
-    const response = await axios.get(GOLD_MARKET_PRICE_API_URL);
+  if (response.data) {
+    const commodityList = response.data.data?.list;
 
-    if (response.data && response.data.length > 0) {
-      const goldData = response.data.flat().find((v) => v.GOLD);
+    if (
+      commodityList &&
+      Array.isArray(commodityList) &&
+      commodityList.length > 0
+    ) {
+      const goldData = commodityList.find(
+        (item) => item.symbol?.toLowerCase()?.replace(/\s/g, '') === 'gold'
+      );
 
       if (goldData) {
         position = {
-          price: Number(goldData.GOLD.LastTradedPrice),
-          change: Number(goldData.GOLD.PercentChange),
-          lastTradedDate: goldData.GOLD.lasttradeddate,
+          price: Number(goldData.lastPrice),
+          change: Number(goldData.priceChangePercentage),
+          lastTradedDate: response.data.data.lastUpdated.replace('| ', ''),
         };
 
         console.info(
@@ -36,8 +44,64 @@ const getMarketPosition = async () => {
     } else {
       console.error(`Invalid market data: ${JSON.stringify(response)}`);
     }
+  } else {
+    console.error(`Invalid response data: ${JSON.stringify(response)}`);
+  }
+
+  return position;
+};
+
+const parseSecondaryApiResponse = (response) => {
+  let position = null;
+
+  if (
+    response.data &&
+    Array.isArray(response.data) &&
+    response.data.length > 0
+  ) {
+    const goldData = response.data.flat().find((v) => v.GOLD);
+
+    if (goldData) {
+      position = {
+        price: Number(goldData.GOLD.LastTradedPrice),
+        change: Number(goldData.GOLD.PercentChange),
+        lastTradedDate: goldData.GOLD.lasttradeddate,
+      };
+
+      console.info(`Today's gold market position: ${JSON.stringify(position)}`);
+    } else {
+      console.error(
+        `Gold market data not found: ${JSON.stringify(response.data)}`
+      );
+    }
+  } else {
+    console.error(`Invalid response/market data: ${JSON.stringify(response)}`);
+  }
+
+  return position;
+};
+
+const getMarketPosition = async () => {
+  let position = null;
+
+  try {
+    const response = await axios.get(GOLD_MARKET_PRICE_API_URL);
+
+    position = parsePrimaryApiResponse(response);
   } catch (error) {
     console.error(`Error fetching market data: ${JSON.stringify(error)}`);
+  }
+
+  if (!position) {
+    console.info('Primary API failed. Falling back to backup API...');
+
+    try {
+      const response = await axios.get(GOLD_MARKET_PRICE_API_BACKUP_URL);
+
+      position = parseSecondaryApiResponse(response);
+    } catch (error) {
+      console.error(`Error fetching market data: ${JSON.stringify(error)}`);
+    }
   }
 
   return position;
@@ -53,9 +117,9 @@ const init = async () => {
     const today = new Date().toLocaleDateString();
 
     const message =
-      lastTradedDate === today
+      lastTradedDate === today || todayMarketPosition.change === 0
         ? `The price _may_ *${
-            todayMarketPosition.change >= 0 ? 'increase' : 'reduce'
+            todayMarketPosition.change > 0 ? 'increase' : 'reduce'
           }* _approximately_ by *${Math.abs(
             todayMarketPosition.change
           )}%* tomorrow!`
@@ -74,9 +138,13 @@ const init = async () => {
       console.info('Message sent successfully!');
     } else {
       console.error('Message sending failed!');
+
+      process.exit(1);
     }
   } else {
     console.error('Market position is not valid!');
+
+    process.exit(1);
   }
 };
 
